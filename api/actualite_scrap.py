51d056a4d69d89fbaa48524f1f96238821182a86
qosyx <archange.jb@gmail.com>
2020-05-09 13:43:12 +0100

    update

--- api/actualite_scrap.py
+++ api/actualite_scrap.py
@@ -2,6 +2,8 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]), 'scrappe'))
 import actualite_scrapping as actualite_scrapping
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]), 'table'))
+import tableCreated as tableCreated
 
 def scrappingActualiteAndSave():
     result = actualite_scrapping.scrappingActualiteAndSave()
@@ -11,3 +13,7 @@ def getAllActualite():
     result = actualite_scrapping.getAllActualite()
     # print(result[0][0]) 
     return result
+
+def createTable():
+    tableCreated.createTable()
+    return "ok"
--- table/tableCreated.py
+++ table/tableCreated.py
@@ -1,15 +1,21 @@
 import sys
 import os
+from pg import DB
 sys.path.append(os.path.join(os.path.dirname(sys.path[0])))
 import table.my_connexion as connexion
 full_path = os.path.realpath(__file__)
 path, filename = os.path.split(full_path)
 path=path+"/connectParameters.json"
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]), 'common_utils'))
+import getDbPath as getDbPath
+db=connexion.connect(getDbPath.getConnectPath())
+
 #Return all tables
-def allTable(db):
+def allTable():
+    
     return db.get_tables()
 #Creation des tables 
-def createTable(db):
+def createTable():
    
     # db.query("DROP DATABASE IF EXISTS veda")
     # db.query("create database veda")
@@ -33,15 +39,16 @@ def createTable(db):
     )
     
 
-def execution(parameters_path):
-    r=connexion.connect(parameters_path)
-    createTable(r)
+# def execution(parameters_path):
+#     r=connexion.connect(parameters_path)
+#     createTable(r)
 
-def dropTableByName(db,tablename):
-    myquery="drop table "+tablename
-    db.query(myquery)
+# def dropTableByName(db,tablename):
+#     myquery="drop table "+tablename
+#     db.query(myquery)
 if __name__ == '__main__':
+    pass
     # Charger tous les paramètres
-    parameters_path = sys.argv[1]
-    r=connexion.connect(parameters_path)
-    createTable(r)
+    # parameters_path = sys.argv[1]
+    # r=connexion.connect(parameters_path)
+    # createTable(r)
